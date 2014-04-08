92e2956dca79fcac3149edda254dbca2c9f2905e
Michael Marino <mmarino@gmail.com>
2014-04-08 15:36:38 +0200

    Add generate script

---
+++ generate_with_version.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+VERSION=1.4.2
+curl http://code.jquery.com/mobile/${VERSION}/jquery.mobile-${VERSION}.js -o jquery-mobile.js  
+patch -p0 < jquery-mobile.patch
+echo $VERSION
+sed -e s/@VERSION@/$VERSION/g README.md.in > README.md
+sed -e s/@VERSION@/$VERSION/g kanso.json.in > kanso.json 
+
