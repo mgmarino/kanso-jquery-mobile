080aba57af891324c39d875f9e4ac04001c42ba6
Michael Marino <mmarino@gmail.com>
2014-05-05 16:52:13 +0200

    Adding css files.

--- generate_with_version.sh
+++ generate_with_version.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 VERSION=1.4.2
 curl http://code.jquery.com/mobile/${VERSION}/jquery.mobile-${VERSION}.js -o jquery-mobile.js  
+curl http://code.jquery.com/mobile/${VERSION}/jquery.mobile-${VERSION}.css -o jquery-mobile.css  
 patch -p0 < jquery-mobile.patch
 echo $VERSION
 sed -e s/@VERSION@/$VERSION/g README.md.in > README.md
